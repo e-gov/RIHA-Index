3682794841dca5a540009334795ec1449fe4d650
Priit Parmakson <priit.parmakson@gmail.com>
2016-12-23 13:21:41 +0200

    Lisandused

---
+++ js/Utils.js
@@ -0,0 +1,11 @@
+function init() {
+
+  // Hide all submenus
+  $('.alammenyy').hide();
+
+  // Bind toggling handler to submenu switches
+  $('.lyliti').click(function() {
+    var i = $(this).attr('id');
+    $('#alammenyy' + i).toggle();
+  });
+}
